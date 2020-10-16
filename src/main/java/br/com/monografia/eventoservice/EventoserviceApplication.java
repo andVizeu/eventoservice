9d4570ffc9a8a70c2e0313ef6330aa5ee00591fc
andVizeu <and.vizeu@hotmail.com>
2020-10-15 23:09:03 -0300

    Initial commit

---
+++ src/main/java/br/com/monografia/eventoservice/EventoserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.monografia.eventoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventoserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/monografia/eventoservice/EventoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.monografia.eventoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
